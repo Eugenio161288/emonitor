115c14547331369a8dcf98d301db0449a8ee4536
Evgeniy Rotar <eugenio161288@gmail.com>
2020-05-11 10:49:10 +0300

    description of the mongo db provider builder

--- src/Server/Euromonitor.Server.MongoDbProvider/MongoDbProviderBuilder.cs
+++ src/Server/Euromonitor.Server.MongoDbProvider/MongoDbProviderBuilder.cs
@@ -1,7 +1,4 @@
 ﻿using Euromonitor.Server.Interfaces.Database;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Euromonitor.Server.MongoDbProvider
 {
@@ -10,24 +7,68 @@ namespace Euromonitor.Server.MongoDbProvider
     /// </summary>
     public class MongoDbProviderBuilder : IDbProviderBuilder
     {
+        MongoDbProvider _dbProvider = new MongoDbProvider();
+
+        /// <summary>
+        /// Initializes an instance of the MongoDbProviderBuilder.
+        /// </summary>
+        public MongoDbProviderBuilder()
+        {}
+
+        /// <summary>
+        /// Builds MongoDbProvider instance with initializes database name and connection string.
+        /// </summary>
+        /// <returns></returns>
         public IDbProvider Build()
         {
-            throw new NotImplementedException();
+            _dbProvider.Initialize();
+
+            return _dbProvider;
         }
 
+        /// <summary>
+        /// Sets collection name to the db provider.
+        /// </summary>
+        /// <param name="collection">The collection name.</param>
+        /// <returns>Returns MongoDbProviderBuilder instance with defined collection name for provider.</returns>
         public IDbProviderBuilder SetCollection(string collection)
         {
-            throw new NotImplementedException();
+            if (!string.IsNullOrEmpty(collection))
+            {
+                _dbProvider.Collection = collection;
+            }
+
+            return this;
         }
 
+        /// <summary>
+        /// Sets connection string to the db provider.
+        /// </summary>
+        /// <param name="connectionString">The connection string.</param>
+        /// <returns>Returns MongoDbProviderBuilder instance with defined connection string.</returns>
         public IDbProviderBuilder SetConnectionString(string connectionString)
         {
-            throw new NotImplementedException();
+            if (!string.IsNullOrEmpty(connectionString))
+            {
+                _dbProvider.ConnectionString = connectionString;
+            }
+
+            return this;
         }
 
+        /// <summary>
+        /// Sets db name to the db provider.
+        /// </summary>
+        /// <param name="dbName">The dtabase name.</param>
+        /// <returns></returns>
         public IDbProviderBuilder SetDatabase(string dbName)
         {
-            throw new NotImplementedException();
+            if (!string.IsNullOrEmpty(dbName))
+            {
+                _dbProvider.Database = dbName;
+            }
+
+            return this;
         }
     }
 }
