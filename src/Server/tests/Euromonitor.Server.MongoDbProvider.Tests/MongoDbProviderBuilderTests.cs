c9c6fea30bd911299ab766a9ded66b4f1a739c13
Evgeniy Rotar <eugenio161288@gmail.com>
2020-05-11 16:03:08 +0300

    unit tests for db provider builder

---
+++ src/Server/tests/Euromonitor.Server.MongoDbProvider.Tests/MongoDbProviderBuilderTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Euromonitor.Server.MongoDbProvider.Tests
+{
+    public class MongoDbProviderBuilderTests
+    {
+        MongoDbProviderBuilder _dbProviderBuilder;
+
+        public MongoDbProviderBuilderTests()
+        {
+            _dbProviderBuilder = new MongoDbProviderBuilder();
+        }
+
+        [Fact]
+        public void SetCollection_EmptyCollection_NullCollection()
+        {
+            var actualResult = _dbProviderBuilder.SetCollection("").Build().Collection;
+
+            Assert.Null(actualResult);
+        }
+    }
+}
