659d7d24333995046df697d2b976cb232a66a34f
Evgeniy Rotar <eugenio161288@gmail.com>
2020-05-11 18:29:29 +0300

    unit tests for mongo db provider builder

--- src/Server/tests/Euromonitor.Server.MongoDbProvider.Tests/MongoDbProviderBuilderTests.cs
+++ src/Server/tests/Euromonitor.Server.MongoDbProvider.Tests/MongoDbProviderBuilderTests.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Xunit;
+﻿using Xunit;
 
 namespace Euromonitor.Server.MongoDbProvider.Tests
 {
@@ -15,11 +12,99 @@ namespace Euromonitor.Server.MongoDbProvider.Tests
         }
 
         [Fact]
-        public void SetCollection_EmptyCollection_NullCollection()
+        public void SetCollection_EmptyCollection_ReturnsNullCollection()
         {
             var actualResult = _dbProviderBuilder.SetCollection("").Build().Collection;
 
             Assert.Null(actualResult);
+        }
+
+        [Fact]
+        public void SetCollection_NotEmptyCollection_StoresProvidedCollection()
+        {
+            var expectedResult = "Collection_Name";
+
+            var actualResult = _dbProviderBuilder.SetCollection("Collection_Name").Build().Collection;
+
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void SetCollection_NotEmptyCollection_ReturnsNotNullCollection()
+        {
+            var actualResult = _dbProviderBuilder.SetCollection("Collection_Name").Build().Collection;
+
+            Assert.NotNull(actualResult);
+        }
+
+        [Fact]
+        public void SetConnectionString_NotEmptyConnectionString_StoresProvidedConnectionString()
+        {
+            var expectedResult = "connectionString";
+
+            var actualResult = _dbProviderBuilder.SetConnectionString("connectionString").Build().ConnectionString;
+
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void SetConnectionString_NotEmptyConnectionString_NotNullConnectionString()
+        {
+            var actualResult = _dbProviderBuilder.SetConnectionString("connectionString").Build().ConnectionString;
+
+            Assert.NotNull(actualResult);
+        }
+
+        [Fact]
+        public void SetConnectionString_EmptyConnectionString_ReturnsNullConnectionString()
+        {
+            var actualResult = _dbProviderBuilder.SetConnectionString("").Build().ConnectionString;
+
+            Assert.Null(actualResult);
+        }
+
+        [Fact]
+        public void SetDatabase_EmptyDbName_ReturnsNullDatabase()
+        {
+            var actualResult = _dbProviderBuilder.SetDatabase("").Build().Database;
+
+            Assert.Null(actualResult);
+        }
+
+        [Fact]
+        public void SetDatabase_NotEmptyDbName_ReturnsNotNullDbName()
+        {
+            var actualResult = _dbProviderBuilder.SetDatabase("DATABASE_NAME").Build().Database;
+
+            Assert.NotNull(actualResult);
+        }
+
+        [Fact]
+        public void SetDatabase_NotEmptyDbName_StoresProvidedDbName()
+        {
+            var expectedResult = "DATABASE_NAME";
+
+            var actualResult = _dbProviderBuilder.SetDatabase("DATABASE_NAME").Build().Database;
+
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void Build_DefaultSettings_AlwaysNotNullDbProvider()
+        {
+            var dbProvider = _dbProviderBuilder.Build();
+
+            Assert.NotNull(dbProvider);
+        }
+
+        [Fact]
+        public void Build_NotDefaultSettings_ReturnsMongoDbProviderType()
+        {
+            var dbProvider = _dbProviderBuilder.SetCollection("Collection")
+                                .SetDatabase("DATABASE")
+                                .Build();
+
+            Assert.IsType<MongoDbProvider>(dbProvider);
         }
     }
 }
